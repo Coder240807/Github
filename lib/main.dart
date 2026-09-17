4680692119e5c998decad37495b51706bd0d4431
Coder240807 <verma.vigya2007@gmail.com>
2026-09-17 16:34:31 +0530

    feat: Update HomeScreen layout with new sections and items

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
           const Padding(
             padding: EdgeInsets.only(right: 12, left: 4),
             child: CircleAvatar(
-              radius: 16,
+              radius: 14,
               backgroundColor: Colors.white,
               child: Text('H', style: TextStyle(color: Colors.black)),
             ),
@@ -62,8 +62,89 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [const SizedBox(height: 8)],
+          children: [
+            const SizedBox(height: 8),
+            _Section(title: 'My Work'),
+            _Item(
+              color: const Color(0xFF2EA44F),
+              icon: OctIcons.issue_opened_24,
+              label: 'Issues',
+            ),
+            _Item(
+              color: const Color(0xFF1F6FEB),
+              icon: OctIcons.git_pull_request_24,
+              label: 'Pull request',
+            ),
+            _Item(
+              color: const Color(0xFF8957E5),
+              icon: OctIcons.comment_discussion_24,
+              label: 'Discussions',
+            ),
+            _Item(
+              color: const Color(0xFF6E7681),
+              icon: OctIcons.project_24,
+              label: 'Projects',
+            ),
+            _Item(
+              color: Color(0xFF30363D),
+              icon: OctIcons.repo_24,
+              label: 'Top Repositories',
+            ),
+            _Item(
+              color: const Color(0xFFDB6D28),
+              icon: OctIcons.organization_24,
+              label: 'Organizations',
+            ),
+            _Item(
+              color: const Color(0xFFD4A72C),
+              icon: OctIcons.star_24,
+              label: 'Starred',
+            ),
+            const Divider(color: Color(0xFF21262D), height: 32),
+
+            const Padding(
+              padding: EdgeInsetsGeometry.symmetric(
+                horizontal: 16,
+                vertical: 10,
+              ),
+              child: Text(
+                'Favourites',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        backgroundColor: const Color(0xFF0D1117),
+        selectedItemColor: const Color(0xFF2F81F7),
+        unselectedItemColor: const Color(0xFF8B949E),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(OctIcons.home_24),
+            activeIcon: Icon(OctIcons.home_fill_24),
+            label: 'Home',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(OctIcons.inbox_24),
+            activeIcon: Icon(OctIcons.inbox_fill_24),
+            label: 'Inbox',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(OctIcons.telescope_24),
+            activeIcon: Icon(OctIcons.telescope_fill_24),
+            label: 'Explore',
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(OctIcons.copilot_24),
+            label: 'Copilot',
+          ),
+        ],
       ),
     );
   }
@@ -71,9 +152,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class _Section extends StatelessWidget {
   final String title;
-  final bool showMore;
 
-  const _Section({required this.title, required this.showMore});
+  const _Section({required this.title});
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +166,6 @@ class _Section extends StatelessWidget {
             title,
             style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          if (showMore) const Icon(Icons.more_horiz, color: Colors.grey),
         ],
       ),
     );
